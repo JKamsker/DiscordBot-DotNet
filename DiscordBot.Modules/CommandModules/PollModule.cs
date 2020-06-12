640b579acfb97e1ccaaf2808a3531cbdb9ea19c9
Jonas Kamsker <11245306+JKamsker@users.noreply.github.com>
2020-06-12 15:10:18 +0200

    Poll initial

---
+++ DiscordBot.Modules/CommandModules/PollModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace DiscordBot.Modules.CommandModules
+{
+    // -poll "Title" "Eins" "Zweites Zwei" "Drittens"
+
+    public class PollModule : ModuleBase
+    {
+        [Command("poll")]
+        public async Task CreatePoll(params string[] options)
+        {
+            foreach (var option in options)
+            {
+                await ReplyAsync(option);
+            }
+        }
+    }
+}
